48de838fe28b17f4bf386b2ff835a38bc23d1a4d
Soanlv <soanpr12@gmail.com>
2020-04-24 10:15:48 +0700

    Facebook Login

--- flutter_exploration/lib/main.dart
+++ flutter_exploration/lib/main.dart
@@ -74,6 +74,7 @@ class _MyAppState extends State<MyApp> {
                       OutlineButton(
                         child: Text("Logout"),
                         onPressed: () {
+                          
                           _logout();
                         },
                       )
